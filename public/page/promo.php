565a70ab5045c1b7b34815013c5132bdd272d029
CookieeJar16 <rafijayalaksana@gmail.com>
2024-12-05 17:02:36 +0700

    Bug Fix

--- public/page/promo.php
+++ public/page/promo.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Komunitas LuarSekolah</title>
-    <link rel="stylesheet" href="./promo.css">
+    <title>Promo LuarSekolah</title>
+    <link rel="stylesheet" href="../css/promo.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
@@ -199,6 +199,6 @@
   </div>
 </footer>
 
-<script src="./promo.js"></script>
+<script src="../js/promo.js"></script>
 </body>
 </html>
